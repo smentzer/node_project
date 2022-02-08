92a06cabf31cdc8043bda9f0614ee8da94db1254
Cayde6 <stephaniementzer@gmail.com>
2022-02-07 21:04:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const app = express();
+//Load HTTP module
+const http = require("http");
+const hostname = '127.0.0.1';
 const port = 3000;
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
+//Create HTTP server and listen on port 3000 for requests
+const server = http.createServer((req, res) => {
+
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
 });
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}!`)
+//listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
+
