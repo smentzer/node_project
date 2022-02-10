b386b13ce53ddec4246a5453bb6d485301d99f98
Cayde6 <stephaniementzer@gmail.com>
2022-02-09 19:41:31 -0500

    replaced code with express code

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-//Load HTTP module
-const http = require("http");
-const hostname = '127.0.0.1';
+// Load Express module
+const express = require('express');
+
+// Create an Express object
+const app = express();
 const port = 3000;
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
-
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+// Setup the Get Route
+app.get('/', function(req, res) {
+  res.send('Hello World!')
 });
 
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// list for request on port 3000
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}!`)
 });
+
+
 
